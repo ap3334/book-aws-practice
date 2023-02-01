7e5df8dbc17b61f9295219745809f9645ff33fe1
ap3334 <chwil2989@naver.com>
2023-02-01 16:34:17 +0900

    init

---
+++ src/test/java/com/example/bookaws/BookAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
